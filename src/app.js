d81e74fa923400ecf8ca9b8f8f0b2374d1460669
chaninwit <120734528+chaninwit@users.noreply.github.com>
2023-06-20 14:44:09 +0700

    api-google-login

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const authRoute = require("./routes/authRoute");
 const cors = require("cors");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const passportSetup = require("../passport");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 const notFoundMiddleware = require("./middlewares/notFound");
 const errorMiddleware = require("./middlewares/error");
-const boardsRoute = require("./route/boardsRoute");
+const boardsRoute = require("./routes/boardsRoute");
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -20,6 +24,17 @@ app.use(
     },
   })
 );
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cyberwolve"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
@@ -29,5 +44,16 @@ app.use("/boards", boardsRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
+
+app.use(
+  cors({
+    orgin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use("/auth", authRoute);
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,9 +1,44 @@
 const express = require("express");
 
-const authController = require("../controllers/authController")
+const authController = require("../controllers/authController");
 
 const router = express.Router();
+const passport = require("passport");
 
-router.post("/register", authController.register)
+router.post("/register", authController.register);
+
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: "SuccessFully Loged In",
+      user: req.user,
+    });
+  } else {
+    res.status(403).json({ error: true, message: "Not Authorized" });
+  }
+});
+
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    error: true,
+    message: "Log in failure",
+  });
+});
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
+
+router.get("/google", passport.authenticate("google", ["profile", "email"]));
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(process.env.CLIENT_URL);
+});
 
 module.exports = router;
