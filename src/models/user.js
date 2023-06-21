9edd3487b7c96f609505057c3792442d4a88f14b
tharathep-k <sun.tharathep@gmail.com>
2023-06-21 17:30:32 +0700

    finish:add models and paranoid

--- src/models/user.js
+++ src/models/user.js
@@ -94,7 +94,15 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       onDelete: "RESTRICT",
-    })
+    });
+
+    User.hasMany(models.BoardMember, {
+      foreignKey: {
+        name: "userId",
+        allowNull: false,
+      },
+      onDelete: "RESTRICT",
+    });
   };
 
   return User;
