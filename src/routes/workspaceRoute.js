e22cc5bb335b3600029012476f2c8e1abeb1118b
Nayza007 <zerkzvn@gmail.com>
2023-06-25 17:53:59 +0700

    config:1

--- src/routes/workspaceRoute.js
+++ src/routes/workspaceRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const workSpaceController = require("../controllers/workspaceController");
+const authenticate = require("../middlewares/authenticate");
 const router = express.Router();
 
-router.get("/:userId", workSpaceController.getAllWorkspaces);
+router.get("/user/:userId", authenticate, (req, res, next) => res.json("hi"));
 
 module.exports = router;
