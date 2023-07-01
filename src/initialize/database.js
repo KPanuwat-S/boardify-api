451b6932878b0f5c0f48c70aa2b27554d038dd1f
tharathep-k <sun.tharathep@gmail.com>
2023-07-01 21:49:46 +0700

    fix:alter-true

--- src/initialize/database.js
+++ src/initialize/database.js
@@ -1,7 +1,3 @@
 const { sequelize } = require("../models");
 
-<<<<<<< HEAD
 sequelize.sync({ alter : true });
-=======
-sequelize.sync({ alter: true });
->>>>>>> 61267c4b37e31104d40a387feb6717257bd6daaf
