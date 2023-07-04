17676c3beda8d3179a52f48de4623875e5a30b90
KPanuwat-S <panuwatbood@gmail.com>
2023-07-04 19:12:48 +0700

    merge develop

--- src/routes/boardsRoute.js
+++ src/routes/boardsRoute.js
@@ -11,18 +11,28 @@ router.post("/", boardsController.createBoard);
 
 // Get all boards in the workspace
 router.get("/:workspaceId", boardsController.getAllBoardsByWorkspaceId);
-
+router.get("/board/:id", boardsController.getOneBoard);
+router.patch("/board/:id", boardsController.editBoardName);
 router.get("/cards/:id", cardsController.getCardsByBoardId);
 router.post("/cards/:id", cardsController.addCard);
 router.patch("/updateCard/", cardsController.updateNameCard);
 router.delete("/cards/:id", cardsController.deleteCard);
+router.patch("/cards/card/:id", cardsController.updateCardName);
 router.patch("/test", cardsController.test);
 router.patch("/updateCard/:id", cardsController.updateNameCard);
+
+// router.post("/tasks/member", taskController.addMeToTask);
+
+router.get("/tasks/members/all/:id", taskController.getMembersInTask);
+router.get("/tasks/:id", taskController.getTaskById);
+
+router.post("/tasks/members/me", taskController.addMeToTask);
+router.delete("/tasks/members/me/:id", taskController.removeMeFromTask);
 
 router.post("/tasks/checklists", taskController.addChecklist);
 router.post("/tasks/:id", taskController.addTask);
 router.patch("/tasks/checklists", taskController.editChecklist);
-router.get("/tasks/:id", taskController.getTaskById);
+
 router.patch("/tasks/:id", taskController.updateTask);
 router.delete("/tasks/:id", taskController.deleteTaskById);
 router.delete("/tasks/checklists/:id", taskController.deleteChecklistById);
